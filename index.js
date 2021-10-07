7091520407f8276cb33fa486a530d768e7c2cae1
mconlon22 <47027126+mconlon22@users.noreply.github.com>
2021-10-07 09:15:42 +0100

    seperated the end points

--- index.js
+++ index.js
@@ -10,30 +10,26 @@ app.use(express.json())    // <==== parse request body as JSON
 
 app.listen(8080)
 
-app.get('/', (req, res) => {
+app.get('/Authenticate', (req, res) => {
   
   const reqData=req.body
 
-
-    if(reqData.payload.contacts[0].hasOwnProperty('bankAccount')){
-      if(failValues.FAIL_BANK_WIZARD_VALUES.includes(responses.FAILED_BANK_WIZARD.decisionElements[0].decision)){
-        res.status(200).send(responses.FAILED_BANK_WIZARD)
-      }
-  }
-  else if(reqData.payload.hasOwnProperty('source')){
-      res.status(200).send(responses.PEPS)
-    
-}
-else{  
-  console.log(failValues.FAIL_AUTH_VALUES)
-    if(failValues.FAIL_AUTH_VALUES.includes(responses.FAILED_BANK_WIZARD.decisionElements[0].decision)){
-       res.status(200).send(responses.FAILED_AUTH)
-    }
-    else{
-      res.status(200).send(responses.FAILED_AUTH)
-    }
-  }
+  res.status(200).send(responses.FAILED_AUTH)
 }
   );
+  app.get('/Peps', (req, res) => {
+  
+    const reqData=req.body
+  
+    res.status(200).send(responses.PEPS)  
+  }
+    );
+    app.get('/BankWizard', (req, res) => {
+  
+      const reqData=req.body
+      
+      res.status(200).send(responses.FAILED_BANK_WIZARD)
+    }
+      );
 
 
